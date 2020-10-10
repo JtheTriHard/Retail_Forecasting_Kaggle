148f8cf73927adbb26e155b795d931ad4ec33662
Joey <jgronovi@ucsd.edu>
2020-10-10 15:45:47 -0700

    Updated data visualizations.
    Will be moved to separate file later.

--- Python/Data_Processing.py
+++ Python/Data_Processing.py
@@ -11,6 +11,7 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from statsmodels.tsa.stattools import adfuller
 from itertools import compress
 from googletrans import Translator
@@ -37,6 +38,10 @@ n_items = df_items.shape[0]
 
 # Check for missing values
 all_dfs = [df_items, df_itemcategories, df_shops, df_train, df_test, df_sample]
+
+# Convert str to datetime and sort
+df_train['date'] = pd.to_datetime(df_train['date'], format = '%d.%m.%Y')
+df_train = df_train.sort_values(['shop_id','date'], ascending = [True,True]).reset_index(drop=True)
 
 # Translate desc. from Russian to English
 translator = Translator()
@@ -65,10 +70,6 @@ for i in range(len(all_categories)):
 df_items = pd.merge(df_items, df_itemcategories[['item_category_id','new_category']], on='item_category_id')
 df_train = pd.merge(df_train, df_items[['item_id','new_category']], on='item_id')
 
-# Convert str to datetime and sort
-df_train['date'] = pd.to_datetime(df_train['date'], format = '%d.%m.%Y')
-df_train = df_train.sort_values(['shop_id','date'], ascending = [True,True]).reset_index(drop=True)
-
 # Aggregate monthly sales of shop-item pairings
 df_train['sales']= df_train['item_price'] * df_train['item_cnt_day']
 df_sales = df_train.groupby(['date_block_num', 'shop_id','item_id','new_category'],as_index=False).agg({'sales':'sum'})
@@ -87,19 +88,83 @@ df_shopcategorysales = df_shopcategorysales.sort_values(['shop_id','new_category
 df_shopsales = df_sales.groupby(['date_block_num','shop_id'],as_index=False).agg({'sales':'sum'})
 df_shopsales = df_shopsales.sort_values(['shop_id','date_block_num'], ascending = [True,True]).reset_index(drop=True)
 
-
+# Fill missing months with zero
+# where period = month, filler = 0
+def fill_missing_dates(df, period, filler):
+    df_out = df.copy()
+    return df_out
+    
 # ADF Test for Stationarity
-# For total sales (result: not stationary)
+# For total sales
 result = adfuller(df_total['sales'].values, autolag=None)
 print('ADF Statistic: %f' % result[0])
 print('p-value: %f' % result[1])
 print('Critical Values:')
 for key, value in result[4].items():
 	print('\t%s: %.3f' % (key, value))
-# Per shop (result: stationary)
+# Per shop
 adf_results = []
 for i in range(n_shops):
     sales = df_sales.loc[df_sales['shop_id']==i,'sales']
     result = adfuller(sales.values, autolag=None)
     adf_results.append(result)
 adf_results = pd.DataFrame(adf_results, columns = ['ADF stat','p-value','used lag','nobs','critical values'])
+
+# De-trending: 
+
+# De-seasonalization:
+
+# =============================================================================
+# DATA VISUALIZATION
+# Move to separate script later
+
+sns.set(style='whitegrid')
+cat_order = ['games','consoles','gifts','movies','accessories','music','cards','programs','books','other']
+
+# Total monthly sales:
+plt.figure()
+g = sns.lineplot(x='date_block_num', y='sales', data=df_total, 
+                  palette=sns.color_palette('Set2'))
+g.set(xlabel='Month', ylabel='Monthly Sales ($)', title = 'Total Monthly Sales')
+
+# Monthly sales per store:
+# Shops with little observations: 0, 1, 8, 9, 11, 20, 23, 36
+def plot_shop(df, shop, xaxis, yaxis):
+    df = df[df['shop_id']==shop]
+    plt.figure()
+    g = sns.lineplot(x=xaxis, y=yaxis, data=df,
+                 palette=sns.color_palette('Set2'))
+    g.legend(['Shop ' + str(i)])
+    g.set(xlabel='Month', ylabel='Monthly Sales ($)', title = 'Monthly Sales Per Shop ID')
+# As separate plots
+#for i in range(n_shops):
+#    plot_shop(df_shopsales, i, 'date_block_num', 'sales')
+# As subplot grid
+plt.figure()
+g = sns.relplot(x='date_block_num', y='sales', col='shop_id', col_wrap=6, 
+                 kind='line', data=df_shopsales)
+g.set(xlabel='Month', ylabel='Monthly Sales ($)')
+
+# Monthly sales per category:
+# As single plot
+plt.figure()
+g = sns.lineplot(x='date_block_num', y='sales', hue ='new_category', 
+                  hue_order = cat_order,
+                  data=df_categorysales, palette=sns.color_palette('Paired', len(cat_order)))
+g.legend(loc='right', bbox_to_anchor=(1.35, 0.5), ncol=1)
+g.set(xlabel='Month', ylabel='Monthly Sales ($)', title = 'Monthly Sales Per Category')
+# As subplot grid with all other categories as silhouettes
+plt.figure()
+g = sns.relplot(
+    data=df_categorysales,
+    x="date_block_num", y="sales", col="new_category", hue="new_category", hue_order = cat_order,
+    kind="line", palette="deep", linewidth=4, col_wrap=5, legend=False)
+for new_category, ax in g.axes_dict.items():
+    ax.text(.8, .85, new_category, transform=ax.transAxes, fontweight="bold")
+    sns.lineplot(
+        data=df_categorysales, x="date_block_num", y="sales", units="new_category",
+        estimator=None, color=".7", linewidth=1, ax=ax,
+    )
+g.set_titles("")
+g.set(xlabel='Month', ylabel='Monthly Sales ($)')
+g.tight_layout()
