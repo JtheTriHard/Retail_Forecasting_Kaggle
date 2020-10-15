505c59fc84a7e3f2c1fc4c3ed88542db3eea934f
Joey <jgronovi@ucsd.edu>
2020-10-14 22:36:35 -0700

    Update Data_Processing.py
    Additional cleaning. Coordinates of shops.

--- Python/Data_Processing.py
+++ Python/Data_Processing.py
@@ -15,6 +15,7 @@ import seaborn as sns
 from statsmodels.tsa.stattools import adfuller
 from itertools import compress
 from googletrans import Translator
+from geopy.geocoders import Nominatim
 
 # Confirm working directory
 print("Working Directory: ", os.getcwd())
@@ -36,19 +37,15 @@ n_shops = df_shops.shape[0]
 n_categories = df_itemcategories.shape[0]
 n_items = df_items.shape[0]
 
-# Check for missing values
-all_dfs = [df_items, df_itemcategories, df_shops, df_train, df_test, df_sample]
-
+# Remove returns/cancellations
+df_train = df_train[df_train['item_cnt_day'] >= 0]
 # Convert str to datetime and sort
 df_train['date'] = pd.to_datetime(df_train['date'], format = '%d.%m.%Y')
 df_train = df_train.sort_values(['shop_id','date'], ascending = [True,True]).reset_index(drop=True)
 
-# Translate desc. from Russian to English
+# Translate item categories from Russian to English
 translator = Translator()
-time_start = time.time()
 df_itemcategories['item_category_name'] = df_itemcategories['item_category_name'].apply(translator.translate, src='ru', dest='en').apply(getattr, args=('text',))
-time_category = time.time() - time_start
-
 # Condense item_category_name
 accessories = list(range(8))
 consoles = list(range(10,18))
@@ -65,35 +62,72 @@ new_category_names = ['accessories', 'consoles', 'games', 'cards', 'movies', 'bo
 df_itemcategories['new_category'] = None
 for i in range(len(all_categories)):
     df_itemcategories.loc[df_itemcategories['item_category_id'].isin(all_categories[i]), 'new_category'] = new_category_names[i]
-
 # Join new categories to main dataframe
 df_items = pd.merge(df_items, df_itemcategories[['item_category_id','new_category']], on='item_category_id')
 df_train = pd.merge(df_train, df_items[['item_id','new_category']], on='item_id')
 
+# Translate shop names from Russian to English
+#df_shops['shop_name'] = df_shops['shop_name'].apply(translator.translate, src='ru', dest='en').apply(getattr, args=('text',))
+# Keep only the city of the shop locations
+df_shops['shop_name'] = df_shops['shop_name'].str.replace(r'[^ \nA-Za-z0-9À-ÖØ-öø-ÿЀ-ӿ/]+','')
+df_shops['shop_name'] = df_shops['shop_name'].apply(lambda x: x.split()[0])
+# Manual adjustments due to abbrev
+df_shops['shop_name'].iloc[34:36] = 'Н Новгород'
+df_shops['shop_name'].iloc[39:42] = 'Ростов На Дону'
+# Retrieve coordinates of shops
+geolocator = Nominatim(user_agent = 'jgronovi@ucsd.edu')
+skip_shops = [9,12,55] # Non-address indices: Offsite Trade, Emergency Online Store, Digital Warehouse
+lat = []
+long = []
+for i in range(len(df_shops['shop_name'])):
+    if i not in skip_shops:
+        cur_loc = geolocator.geocode(df_shops['shop_name'].iloc[i])
+        lat.append(cur_loc.latitude), long.append(cur_loc.longitude)
+    else:
+        lat.append(float('NaN')), long.append(float('NaN'))
+df_shops['lat'] = lat
+df_shops['long'] = long
+
+# Convert date_block_num int to date
+yr1 = '2013'
+mth1 = '01'
+yr2 = '2015'
+mth2 = '11' # stops with the month before
+def month_as_date(df, start_year, start_month, end_year, end_month):
+    date_interval = pd.date_range(start_year + '-' + start_month, 
+                                  end_year + '-' + end_month, freq='M').to_list()
+    date_map = {}
+    for i in range(34):
+        date_map[i] = date_interval[i]
+    #df['date'] = df['date_block_num'].map(date_map)
+    df = df.replace({'date_block_num': date_map})
+    return df
+
 # Aggregate monthly sales of shop-item pairings
 df_train['sales']= df_train['item_price'] * df_train['item_cnt_day']
-df_sales = df_train.groupby(['date_block_num', 'shop_id','item_id','new_category'],as_index=False).agg({'sales':'sum'})
+df_sales = df_train.groupby(['date_block_num', 'shop_id','item_id','new_category'],as_index=False).agg({'sales':'sum', 'item_cnt_day':'sum'})
+df_sales = month_as_date(df_sales, yr1, mth1, yr2, mth2)
 # Aggregate total monthly sales
-df_total = df_sales.groupby(['date_block_num'],as_index=False).agg({'sales':'sum'})
+df_total = df_sales.groupby(['date_block_num'],as_index=False).agg({'sales':'sum', 'item_cnt_day':'sum'})
 # Aggregate monthly sales per item
-df_itemsales = df_sales.groupby(['date_block_num','item_id'],as_index=False).agg({'sales':'sum'})
+df_itemsales = df_sales.groupby(['date_block_num','item_id'],as_index=False).agg({'sales':'sum', 'item_cnt_day':'sum'})
 df_itemsales = df_itemsales.sort_values(['item_id','date_block_num'], ascending = [True,True]).reset_index(drop=True)
 # Aggregate monthly sales per item category
-df_categorysales = df_sales.groupby(['date_block_num','new_category'],as_index=False).agg({'sales':'sum'})
+df_categorysales = df_sales.groupby(['date_block_num','new_category'],as_index=False).agg({'sales':'sum', 'item_cnt_day':'sum'})
 df_categorysales = df_categorysales.sort_values(['new_category','date_block_num'], ascending = [True,True]).reset_index(drop=True)
 # Aggregate monthly sales of shop-category pairings
-df_shopcategorysales = df_sales.groupby(['date_block_num', 'shop_id','new_category'],as_index=False).agg({'sales':'sum'})
+df_shopcategorysales = df_sales.groupby(['date_block_num', 'shop_id','new_category'],as_index=False).agg({'sales':'sum', 'item_cnt_day':'sum'})
 df_shopcategorysales = df_shopcategorysales.sort_values(['shop_id','new_category','date_block_num'], ascending = [True,True,True]).reset_index(drop=True)
 # Aggregate monthly sales per shop
-df_shopsales = df_sales.groupby(['date_block_num','shop_id'],as_index=False).agg({'sales':'sum'})
+df_shopsales = df_sales.groupby(['date_block_num','shop_id'],as_index=False).agg({'sales':'sum', 'item_cnt_day':'sum'})
 df_shopsales = df_shopsales.sort_values(['shop_id','date_block_num'], ascending = [True,True]).reset_index(drop=True)
 
-# Fill missing months with zero
+# UNFINISHED: Fill missing months with zero
 # where period = month, filler = 0
 def fill_missing_dates(df, period, filler):
     df_out = df.copy()
     return df_out
-    
+
 # ADF Test for Stationarity
 # For total sales
 result = adfuller(df_total['sales'].values, autolag=None)
@@ -125,7 +159,7 @@ cat_order = ['games','consoles','gifts','movies','accessories','music','cards','
 plt.figure()
 g = sns.lineplot(x='date_block_num', y='sales', data=df_total, 
                   palette=sns.color_palette('Set2'))
-g.set(xlabel='Month', ylabel='Monthly Sales ($)', title = 'Total Monthly Sales')
+g.set(xlabel='Month', ylabel='Monthly Sales (RUB)', title = 'Total Monthly Sales')
 
 # Monthly sales per store:
 # Shops with little observations: 0, 1, 8, 9, 11, 20, 23, 36
@@ -135,7 +169,7 @@ def plot_shop(df, shop, xaxis, yaxis):
     g = sns.lineplot(x=xaxis, y=yaxis, data=df,
                  palette=sns.color_palette('Set2'))
     g.legend(['Shop ' + str(i)])
-    g.set(xlabel='Month', ylabel='Monthly Sales ($)', title = 'Monthly Sales Per Shop ID')
+    g.set(xlabel='Month', ylabel='Monthly Sales (RUB)', title = 'Monthly Sales Per Shop ID')
 # As separate plots
 #for i in range(n_shops):
 #    plot_shop(df_shopsales, i, 'date_block_num', 'sales')
@@ -143,16 +177,18 @@ def plot_shop(df, shop, xaxis, yaxis):
 plt.figure()
 g = sns.relplot(x='date_block_num', y='sales', col='shop_id', col_wrap=6, 
                  kind='line', data=df_shopsales)
-g.set(xlabel='Month', ylabel='Monthly Sales ($)')
+g.set(xlabel='Month', ylabel='Monthly Sales (RUB)')
+g.set_xticklabels(rotation=-45, horizontalalignment = 'left')
 
 # Monthly sales per category:
 # As single plot
 plt.figure()
 g = sns.lineplot(x='date_block_num', y='sales', hue ='new_category', 
                   hue_order = cat_order,
-                  data=df_categorysales, palette=sns.color_palette('Paired', len(cat_order)))
+                  data=df_categorysales, palette=sns.color_palette('deep', len(cat_order)))
 g.legend(loc='right', bbox_to_anchor=(1.35, 0.5), ncol=1)
-g.set(xlabel='Month', ylabel='Monthly Sales ($)', title = 'Monthly Sales Per Category')
+plt.setp(g.get_xticklabels(), rotation=-45, horizontalalignment='left')
+g.set(xlabel='Month', ylabel='Monthly Sales (RUB)', title = 'Monthly Sales Per Category')
 # As subplot grid with all other categories as silhouettes
 plt.figure()
 g = sns.relplot(
@@ -166,5 +202,6 @@ for new_category, ax in g.axes_dict.items():
         estimator=None, color=".7", linewidth=1, ax=ax,
     )
 g.set_titles("")
-g.set(xlabel='Month', ylabel='Monthly Sales ($)')
+g.set(xlabel='Month', ylabel='Monthly Sales (RUB)')
+g.set_xticklabels(rotation=-45, horizontalalignment = 'left')
 g.tight_layout()
